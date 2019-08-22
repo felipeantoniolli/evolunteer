287d5796d50098537a3c20e7b7367e1c71ead8b9
felipeantoniolli <contato.felipetrindade@gmail.com>
2019-08-22 03:39:03 +0000

    Adição da Factory e Seeder do User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User;
+use App\Model\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -18,8 +18,20 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'user' => $faker->unique()->name,
+        'telephone' => $faker->numberBetween($min = 000000001, $max = 999999999),
+        'type' => $faker->numberBetween($min = 1, $max = 2),
+        'cep' => $faker->numberBetween($min = 0000001, $max = 9999999),
+        'street' => $faker->streetName,
+        'number' => $faker->numberBetween($min = 1, $max = 100),
+        'city' => $faker->city,
+        'state' => $faker->stateAbbr,
+        'complement' => null,
+        'reference' => null,
+        'active' => 1,
+        'secondary_telephone' => null,
+        'secondary_email' => null,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
--- routes/api.php
+++ routes/api.php
@@ -20,35 +20,35 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post("user/create", "UserController@create");
 Route::patch("user/update/{user}", "UserController@update");
 Route::get('user/find-all', "UserController@findAll");
-Route::get('user/find-by-id/{user}', "UserController@findById");
+Route::get('user/find/{user}', "UserController@findById");
 Route::delete('user/delete/{user}', "UserController@destroy");
 
 Route::post("volunteer/create", "VolunteerController@create");
 Route::patch("volunteer/update/{volunteer}", "VolunteerController@update");
 Route::get("volunteer/find-all", "VolunteerController@findAll");
-Route::get("volunteer/find-by-id/{volunteer}", "VolunteerController@findById");
+Route::get("volunteer/find/{volunteer}", "VolunteerController@findById");
 Route::delete("volunteer/delete/{volunteer}", "VolunteerController@destroy");
 
 Route::post("institution/create", "InstitutionController@create");
 Route::patch("institution/update/{institution}", "InstitutionController@update");
 Route::get("institution/find-all", "InstitutionController@findAll");
-Route::get("institution/find-by-id/{institution}", "InstitutionController@findById");
+Route::get("institution/find/{institution}", "InstitutionController@findById");
 Route::delete("institution/delete/{institution}", "InstitutionController@destroy");
 
 Route::post("rating/create", "RatingController@create");
 Route::patch("rating/update/{rating}", "RatingController@update");
 Route::get("rating/find-all", "RatingController@findAll");
-Route::get("rating/find-by-id/{rating}", "RatingController@findById");
+Route::get("rating/find/{rating}", "RatingController@findById");
 Route::delete("rating/delete/{rating}", "RatingController@destroy");
 
 Route::post("work/create", "WorkController@create");
 Route::patch("work/update/{work}", "WorkController@update");
 Route::get("work/find-all", "WorkController@findAll");
-Route::get("work/find-by-id/{work}", "WorkController@findById");
+Route::get("work/find/{work}", "WorkController@findById");
 Route::delete("work/delete/{work}", "WorkController@destroy");
 
 Route::post("solicitation/create", "SolicitationController@create");
 Route::patch("solicitation/update/{solicitation}", "SolicitationController@update");
 Route::get("solicitation/find-all", "SolicitationController@findAll");
-Route::get("solicitation/find-by-id/{solicitation}", "SolicitationController@findById");
+Route::get("solicitation/find/{solicitation}", "SolicitationController@findById");
 Route::delete("solicitation/delete/{solicitation}", "SolicitationController@destroy");
