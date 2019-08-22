017820c9e468f1e533e3c0f121e9467bd0a3820c
felipeantoniolli <contato.felipetrindade@gmail.com>
2019-08-22 04:00:12 +0000

    Alterações no UserFactory e altrado o dockerfile para executar a criação da database evolunteer na execução do gitpod

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ use Faker\Generator as Faker;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'email' => $faker->unique()->safeEmail,
-        'user' => $faker->unique()->name,
+        'user' => $faker->unique()->userName,
         'telephone' => $faker->numberBetween($min = 000000001, $max = 999999999),
         'type' => $faker->numberBetween($min = 1, $max = 2),
         'cep' => $faker->numberBetween($min = 0000001, $max = 9999999),
@@ -33,7 +33,7 @@ $factory->define(User::class, function (Faker $faker) {
         'secondary_telephone' => null,
         'secondary_email' => null,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'password' => $faker->sha256,
+        'remember_token' => Str::random(10)
     ];
 });
